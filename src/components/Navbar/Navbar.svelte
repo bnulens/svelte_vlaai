ec41e02eb49754b07d7ed69709d6c4696b96d2f5
bnulens <brechtnulens@gmail.com>
2022-03-31 12:33:51 +0200

    Separated NavbarItem into separate component - generic Navbar

---
+++ src/components/Navbar/Navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+  import NavbarItem from "./NavbarItem.svelte";
+</script>
+
+<div class="component-wrapper" id="home">
+  <div class="navbar-wrapper">
+    <NavbarItem link={"#home"} title={"Home"} />
+    <nav class="main-navbar">
+      <NavbarItem link={"#about"} title={"About"} />
+      <NavbarItem link={"#context"} title={"Context"} />
+      <NavbarItem link={"#guide"} title={"Guide"} />
+      <NavbarItem link={"#contact"} title={"Contact"} />
+    </nav>
+  </div>
+</div>
+
+<style lang="scss">
+  .component-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    .navbar-wrapper {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 40px;
+
+      .main-navbar {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+
+    &:after {
+      display: block;
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 50%;
+      width: 100vw;
+      height: 40px;
+      background-color: black;
+      transform: translate(-50%);
+      z-index: -10;
+    }
+  }
+</style>
---
+++ src/components/Navbar/NavbarItem.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let link;
+  export let title;
+</script>
+
+<a class="main-navbar-item" href={link}>{title}</a>
+
+<style lang="scss">
+  .main-navbar-item {
+    color: white;
+    font-size: clamp(8px, 2vw, 14px);
+    margin: 0 12px;
+  }
+</style>
