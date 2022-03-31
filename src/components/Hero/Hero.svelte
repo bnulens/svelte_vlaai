6a2e7299135441357acf2e31e44de2cbd9833cd0
bnulens <brechtnulens@gmail.com>
2022-03-31 12:32:44 +0200

    Started Hero component - added Gallery + nodes to navigate the Gallery

---
+++ src/components/Hero/Hero.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Gallery from "./Gallery.svelte";
+  import HeroNavigation from "./HeroNavigation.svelte";
+</script>
+
+<div class="component-wrapper" id="hero">
+  <div class="hero-wrapper">
+    <Gallery />
+    <HeroNavigation />
+  </div>
+</div>
+
+<style lang="scss">
+  .component-wrapper {
+    position: relative;
+    width: 100vw;
+    height: 65vh;
+    transform: translate(-50%);
+    margin-left: 50%;
+
+    .hero-wrapper {
+      width: 100%;
+      height: 100%;
+      background-color: hsla(120, 100%, 25%, 0.521);
+    }
+  }
+</style>
---
+++ src/components/Hero/HeroNavigation.svelte
@@ -0,0 +1,55 @@
+<div class="hero-navigation-wrapper">
+  <ul class="hero-navigation">
+    <li class="hero-navigation-item" />
+    <li class="hero-navigation-item" />
+    <li class="hero-navigation-item" />
+    <li class="hero-navigation-item" />
+  </ul>
+</div>
+
+<style lang="scss">
+  .hero-navigation-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    position: relative;
+    height: 100%;
+    width: 95%;
+    margin: 0 auto;
+
+    .hero-navigation {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0 auto 24px;
+      width: clamp(90px, 30%, 220px);
+
+      .hero-navigation-item {
+        position: relative;
+        display: block;
+        width: 12px;
+        height: 12px;
+        border: 1px solid white;
+        border-radius: 50%;
+
+        &:hover {
+          cursor: pointer;
+
+          &::after {
+            display: block;
+            position: absolute;
+            content: "";
+            top: 50%;
+            left: 50%;
+            width: 70%;
+            height: 70%;
+            border: none;
+            border-radius: 50%;
+            transform: translate(-50%, -50%);
+            background-color: white;
+          }
+        }
+      }
+    }
+  }
+</style>
