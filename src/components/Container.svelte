d770cf9be487906d94a0b5b43d9034488210f3d3
bnulens <brechtnulens@gmail.com>
2022-03-31 12:31:17 +0200

    Wrapped all components inside Container to strap all children into uniform width

---
+++ src/components/Container.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Navbar from "./Navbar/Navbar.svelte";
+  import Hero from "./Hero/Hero.svelte";
+</script>
+
+<div class="app-container">
+  <Navbar />
+  <Hero />
+</div>
+
+<style lang="scss">
+  .app-container {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    max-width: 95%;
+    margin: 0 auto;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .app-container {
+      max-width: 1080px;
+    }
+  }
+</style>
